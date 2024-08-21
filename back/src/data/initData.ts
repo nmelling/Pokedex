694120d3aba3614bf6f2fae4f4be50499d5ddd5a
nikaula <nicolas.mellinger.pro@gmail.com>
2024-08-21 13:08:06 +0200

    convert initData.mjs to ts file

--- back/src/data/initData.ts
+++ back/src/data/initData.ts
@@ -1,41 +1,65 @@
 import axios, { AxiosResponse } from 'axios';
 import _ from 'lodash';
 import path from 'node:path';
+import fs from 'node:fs';
 
 import { Pokemon } from '../types/pokemon';
+import { ShortcutItem } from '../types/apiResponse/common';
 import { FetchedPokemon } from '../types/apiResponse/pokemons'
 import { FetchedSpecies } from '../types/apiResponse/species'
  
 
 function formatPokemonData(pokemon: FetchedPokemon, specie: FetchedSpecies): Pokemon {
-  const sprites = Object.fromEntries(
-    Object.entries(_.pick(pokemon.sprites, ['back_default', 'back_shiny']))
-    .map(([key, value]) => ([key.replace('back_', ''), value]))
-  )
-
   return {
-    name: (specie.names || []).find(entry => entry.language.name === 'fr').name,
+    name: (specie.names! ?? []).find(entry => entry.language.name === 'fr')?.name || '',
     stats: Object.fromEntries(pokemon.stats.map(entry => [entry.stat.name, entry.base_stat])),
-    sprites,
+    sprites: {
+      default: pokemon.sprites.back_default ?? null,
+      shiny: pokemon.sprites.back_shiny ?? null,
+    },
     types: pokemon.types.map(entry => entry.type.name),
     abilities: pokemon.abilities.map(entry => entry.ability),
   }
 }
 
-async function fetchPokemons(): Promise<Pokemon[]> {
-  let pokemons: Pokemon[] = []
-
+async function axiosFetcher (url: string) {
   try {
-    const response: AxiosResponse<object> = await axios.get('https://pokeapi.co/api/v2/pokemon', {
+    const response: AxiosResponse = await axios.get(url);
+    return response.data
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function fetchPokemons(): Promise<Pokemon[]> {
+  try {
+    const response: AxiosResponse = await axios.get('https://pokeapi.co/api/v2/pokemon', {
       params: {
         limit: 20,
         offset: 0,
       }
     });
+
+    return await Promise.all((response.data.results ?? []).map(async (entry: ShortcutItem) => {
+      const pokemon: FetchedPokemon = await axiosFetcher(entry.url)
+      const species: FetchedSpecies = await axiosFetcher(pokemon.species.url)
+
+      return formatPokemonData(pokemon, species)
+    }))
   } catch (err) {
     console.error(err);
     throw err;
   }
-
-  return pokemons;
 }
+
+async function main(): Promise<void> {
+  const pokemons = await fetchPokemons()
+
+  const sourceFilePath = path.join(__dirname, '../ressources/pokemons.json')
+  fs.writeFileSync(sourceFilePath, JSON.stringify({ pokemons }))
+
+  console.log('populated')
+}
+
+main()
