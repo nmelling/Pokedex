aeafb8a61e00e7423cc47eafd918b2bfdf0e55a2
nikaula <nicolas.mellinger.pro@gmail.com>
2024-09-24 18:17:13 +0200

    lint

--- back/src/data/initData.ts
+++ back/src/data/initData.ts
@@ -5,65 +5,78 @@ import fs from 'node:fs';
 
 import type { Pokemon } from '../types/pokemon.d.ts';
 import type { ShortcutItem } from '../types/apiResponse/common.d.ts';
-import type { FetchedPokemon } from '../types/apiResponse/pokemons.d.ts'
-import type { FetchedSpecies } from '../types/apiResponse/species.d.ts'
- 
+import type { FetchedPokemon } from '../types/apiResponse/pokemons.d.ts';
+import type { FetchedSpecies } from '../types/apiResponse/species.d.ts';
 
-export function formatPokemonData(pokemon: FetchedPokemon, specie: FetchedSpecies): Pokemon {
+export function formatPokemonData(
+  pokemon: FetchedPokemon,
+  specie: FetchedSpecies,
+): Pokemon {
   return {
-    name: (specie.names! ?? []).find(entry => entry.language.name === 'fr')?.name || '',
-    stats: Object.fromEntries(pokemon.stats.map(entry => [entry.stat.name, entry.base_stat])),
+    name:
+      (specie.names ?? []).find((entry) => entry.language.name === 'fr')
+        ?.name || '',
+    stats: Object.fromEntries(
+      pokemon.stats.map((entry) => [entry.stat.name, entry.base_stat]),
+    ),
     sprites: {
       default: pokemon.sprites.back_default ?? null,
       shiny: pokemon.sprites.back_shiny ?? null,
     },
-    types: pokemon.types.map(entry => entry.type.name),
-    abilities: pokemon.abilities.map(entry => entry.ability),
-  }
+    types: pokemon.types.map((entry) => entry.type.name),
+    abilities: pokemon.abilities.map((entry) => entry.ability),
+  };
 }
 
-export async function fetchPokemons(maxCount: number, $batchSize: number): Promise<Pokemon[]> {
+export async function fetchPokemons(
+  maxCount: number,
+  $batchSize: number,
+): Promise<Pokemon[]> {
   const pokemons: Pokemon[] = [];
 
   let rounded = 1;
-  let batchSize = Math.min($batchSize, maxCount);
+  const batchSize = Math.min($batchSize, maxCount);
   if (batchSize < maxCount) rounded = Math.round(maxCount / batchSize);
 
-  const batched = new Array(rounded)
-  .fill(null)
-  .map((_entry, index) => {
+  const batched = new Array(rounded).fill(null).map((_entry, index) => {
     const formatted = {
       limit: batchSize,
       offset: index + index,
     };
-    if (index === rounded - 1) formatted.limit = maxCount - (index * batchSize);
+    if (index === rounded - 1) formatted.limit = maxCount - index * batchSize;
     return formatted;
   });
 
   for (const { limit, offset } of batched) {
     try {
-      const response: AxiosResponse = await axios.get('https://pokeapi.co/api/v2/pokemon', {
-        params: {
-          limit,
-          offset,
-        }
-      });
-  
-      const formattedPokemons: Pokemon[] = await Promise.all((response.data.results ?? []).map(async (entry: ShortcutItem) => {
-        const { data: pokemon }: AxiosResponse  = await axios.get(entry.url);
-        const { data: species }: AxiosResponse = await axios.get(pokemon.species.url);
-  
-        return formatPokemonData(pokemon, species);
-      }))
+      const response: AxiosResponse = await axios.get(
+        'https://pokeapi.co/api/v2/pokemon',
+        {
+          params: {
+            limit,
+            offset,
+          },
+        },
+      );
+
+      const formattedPokemons: Pokemon[] = await Promise.all(
+        (response.data.results ?? []).map(async (entry: ShortcutItem) => {
+          const { data: pokemon }: AxiosResponse = await axios.get(entry.url);
+          const { data: species }: AxiosResponse = await axios.get(
+            pokemon.species.url,
+          );
+
+          return formatPokemonData(pokemon, species);
+        }),
+      );
 
       pokemons.push(...formattedPokemons);
     } catch (err) {
       console.error(err);
       throw err;
     }
-
   }
-  
+
   return pokemons;
 }
 
--- back/src/spec/initData/initData.spec.ts
+++ back/src/spec/initData/initData.spec.ts
@@ -18,8 +18,10 @@ const formattedBulbisaur = {
     speed: 45,
   },
   sprites: {
-    default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
-    shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png',
+    default:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+    shiny:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png',
   },
   types: ['grass', 'poison'],
   abilities: [
@@ -30,38 +32,47 @@ const formattedBulbisaur = {
     {
       name: 'chlorophyll',
       url: 'https://pokeapi.co/api/v2/ability/34/',
-    }
+    },
   ],
 };
 
 describe('format pokemon data', () => {
   it('should format the provided data correctly', () => {
-    expect(formatPokemonData(fetchedPokemon.pokemon, fetchedSpecies.species))
-    .toEqual(formattedBulbisaur);
+    expect(
+      formatPokemonData(fetchedPokemon.pokemon, fetchedSpecies.species),
+    ).toEqual(formattedBulbisaur);
   });
 });
 
-function generateMockResponses (resultNumber: number): { [key: string]: FetchedPokemon | FetchedSpecies | { results: ShortcutItem[] } } {
+function generateMockResponses(
+  resultNumber: number,
+): Record<
+  string,
+  FetchedPokemon | FetchedSpecies | { results: ShortcutItem[] }
+> {
   return {
     'https://pokeapi.co/api/v2/pokemon': {
-      results: new Array(resultNumber).fill({ name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' }),
+      results: new Array(resultNumber).fill({
+        name: 'bulbasaur',
+        url: 'https://pokeapi.co/api/v2/pokemon/1/',
+      }),
     },
     'https://pokeapi.co/api/v2/pokemon/1/': fetchedPokemon.pokemon,
     'https://pokeapi.co/api/v2/pokemon-species/1/': fetchedSpecies.species,
-  }
-};
+  };
+}
 
 describe('fetch pokemons', () => {
-  it('should return the <maxCount> pokemons formatted as expected', async function() {
+  it('should return the <maxCount> pokemons formatted as expected', async function () {
     const mockedResponses = generateMockResponses(2);
 
     spyOn(axios, 'get').and.callFake(<T>(url: string): Promise<T> => {
       return Promise.resolve({ data: mockedResponses[url] } as T);
-    })
+    });
 
     const formattedPokemons = await fetchPokemons(6, 2);
     expect(formattedPokemons.length).toEqual(6);
-    expect(formattedPokemons).toEqual(new Array(6).fill(formattedBulbisaur))
+    expect(formattedPokemons).toEqual(new Array(6).fill(formattedBulbisaur));
     expect(axios.get).toHaveBeenCalledTimes(15);
   });
 
@@ -70,7 +81,7 @@ describe('fetch pokemons', () => {
 
     spyOn(axios, 'get').and.callFake(<T>(url: string): Promise<T> => {
       return Promise.resolve({ data: mockedResponses[url] } as T);
-    })
+    });
 
     const formattedPokemons = await fetchPokemons(6, 10);
     expect(formattedPokemons.length).toEqual(6);
@@ -84,10 +95,10 @@ describe('fetch pokemons', () => {
         message: 'Network error',
         code: '',
         response: undefined,
-      }
+      };
 
       return Promise.reject(error);
-    })
+    });
 
     let formattedPokemons: Pokemon[] = [];
     try {
@@ -98,4 +109,4 @@ describe('fetch pokemons', () => {
     expect(formattedPokemons.length).toEqual(0);
     expect(axios.get).toHaveBeenCalledTimes(1);
   });
-})
+});
